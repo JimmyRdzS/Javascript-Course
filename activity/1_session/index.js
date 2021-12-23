14404825fce82484e8f0a1e437136f317850dcd1
Jaime Rodriguez Solis <jaime_rdez794@live.com.mx>
2021-12-23 16:32:33 -0600

    Ejercicio 3

--- activity/1_session/index.js
+++ activity/1_session/index.js
@@ -10,6 +10,13 @@ console.log("Ejercicio 1");
 */
 
 // Ejercicio 3
+/*
+Enumera 3 herramientas útiles dentro de las herramientas de desarrollo de Google Chrome o Mozila Firefox (inspector, consola, networking…) y explica su funcionamiento.
+Google Chrome - Herramientas de desarrollador:
+1.	Network: En la pestaña de network vamos a poder encontrar todos los elementos que se cargan en un sitio web, así como poder monitorear llamadas a recursos externos, saber su tamaño, tiempo de carga y el status de la respuesta, etc. Cada que se cargue el sitio se van a ir enumerando los elementos mostrando en forma de cascada la información de cada petición o llamada.
+2.	Application: Dentro de este apartado o pestaña de el panel de desarrolladores vamos a poder encontrar múltiples elementos respecto al almacenamiento, cache, servicios en segundo plano, o servicios del sitio web que estemos visualizando, cada que haya alguna interacción o el sitio cuente con un elemento de los mencionados anteriormente, vamos a poder visualizarlo, así como poder modificarlo. Esta herramienta se vuelve muy útil ya que muchos de estos elementos no se pueden visualizar de muchas maneras, por lo que este panel facilita al desarrollador tener el acceso a la información desde una interfaz más sencilla.
+3.	Sources: En esta sección vamos a poder encontrar todos los archivos que se llaman en el proyecto, así como recursos que se solicitan o se utilizan, aún si estos archivos pertenecen a algún CDN o hosting externo. Se mostrarán los recursos en una estructura de árbol, que además de permitir visualizarlos, puedes acceder a estos archivos o descargarlos de una manera más sencilla.
+*/
 
 // Ejercicio 4
 

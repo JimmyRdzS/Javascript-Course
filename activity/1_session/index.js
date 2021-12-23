b01ad1bffde18f415dfbfcef4488d1fdca09c76c
Jaime Rodriguez Solis <jaime_rdez794@live.com.mx>
2021-12-23 15:20:48 -0600

    Ejercicio 2

--- activity/1_session/index.js
+++ activity/1_session/index.js
@@ -3,6 +3,11 @@ import {template} from "./aboutme.js";
 console.log("Ejercicio 1");
 
 // Ejercicio 2
+/*
+¿Qué se ejecuta antes, la llamada a console.log() o el contenido del fichero aboutme.js?
+- Se ejecuta el contenido del fichero aboutme.js, con los breakpoints añadidos se puede confirmar el comportamiento de la ejecución.
+- Se adjunta una impresión de pantalla de los breakpoints y el resultado en consola, el archivo se encuentra en activity > 1_session > ejercicio2.png
+*/
 
 // Ejercicio 3
 

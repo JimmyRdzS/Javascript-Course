3a3c373aabbe52ffd8ae37922f51b28c53de658f
Jaime Rodriguez Solis <jaime_rdez794@live.com.mx>
2021-12-23 15:12:32 -0600

    Agregado aboutme.js y cambios de la Actividad 1 Ejercicio 1

---
+++ activity/1_session/aboutme.js
@@ -0,0 +1,8 @@
+const nombreCompleto = 'Jaime Humberto Rodríguez Solis';
+const profesion = 'Ingeniería en Sistemas Computacionales';
+const puestoActual = 'Senior Software Engineer';
+
+let template = `Hola, me llamo ${nombreCompleto} y me dedico a ${profesion}. Estoy cursando este Máster porque me gustaría trabajar en ${puestoActual}.`;
+console.log(template);
+
+export { template };
--- activity/1_session/index.js
+++ activity/1_session/index.js
@@ -1,5 +1,5 @@
 // Ejercicio 1
-
+import {template} from "./aboutme.js";
 console.log("Ejercicio 1");
 
 // Ejercicio 2

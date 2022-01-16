c0e168ebea6c1046ded34f7ab37728d341b0f101
Jaime Rodriguez Solis <jaime_rdez794@live.com.mx>
2022-01-16 15:09:34 -0600

    Actividad 2 - Ejercicio 1

--- activity/2_session/index.js
+++ activity/2_session/index.js
@@ -1,5 +1,18 @@
 // Ejercicio 1
 
+let person = {
+  name: "Jaime Rodriguez",
+  age: 27,
+  profession: "Developer",
+} 
+
+function getParams(customObject){
+  let keys = Object.keys(customObject);
+  return keys;
+}
+
+console.log(getParams(person));
+
 // Ejercicio 2
 
 // Ejercicio 3
@@ -11,13 +24,13 @@
 // Ejercicio 6
 
 let loginWitUsername = (username, password) => {
-    return new Promise(function (resolve, rejected) {
-      setTimeout(() => {
-        if (username === "admin" && password === "passwd") {
-          resolve("User logged in");
-        } else {
-          rejected("Error: invalid username or password");
-        }
-      }, 200);
-    });
+  return new Promise(function (resolve, rejected) {
+    setTimeout(() => {
+      if (username === "admin" && password === "passwd") {
+        resolve("User logged in");
+      } else {
+        rejected("Error: invalid username or password");
+      }
+    }, 200);
+  });
 };

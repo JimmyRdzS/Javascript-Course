ff349e59ac05e318dfd610b85ba8217af23a7f66
Jaime Rodriguez Solis <jaime_rdez794@live.com.mx>
2022-01-16 16:13:04 -0600

    Actividad 2 - Ejercicio 2

--- activity/2_session/index.js
+++ activity/2_session/index.js
@@ -14,6 +14,55 @@ function getParams(customObject){
 console.log(getParams(person));
 
 // Ejercicio 2
+console.log(this === window);
+
+console.log(this);
+
+function f1(){ return this; }
+console.log(f1());
+
+let obj = {
+  name: "Jaime Rodriguez",
+  f() {
+    console.log('This => ', this.name);
+  }
+};
+obj.f();
+
+let obj2 = {
+  name: "Humberto Solis",
+  f: () => {
+    console.log('This => ', this.name);
+  }
+};
+obj2.f();
+
+var name = "test name"; //Global
+let obj3 = {
+  name: "Humberto Solis",
+  f: () => {
+    console.log('This => ', this.name);
+  }
+};
+obj3.f();
+
+let obj4 = {
+  name: "Jaime",
+  f: function() {
+    return this;
+  }
+};
+console.log(obj4);
+
+let obj5 = {
+  name: "Humberto",
+  f: function() {
+    return this.name;
+  }
+};
+console.log(obj5);
+
+
 
 // Ejercicio 3
 
